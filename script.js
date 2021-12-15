3c6163bdf000a9b5828b744de96c0b05aab13acc
BONAFIDE BOSS <amaanalmir@gmail.com>
2021-12-15 17:22:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ file.open("GET", "imdbpolls.txt");
 file.send();
 file.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
-    document.getElementById('imdbpolls').innerHTML = file.responseText
+    var data = file.responseText
+    document.getElementById(imdbpolls).innerHTML = data.url
   }
 }
