b4e7c2c4a594f4a8766acd7e5484dfba60f56582
BONAFIDE BOSS <amaanalmir@gmail.com>
2021-12-16 03:41:09 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -153,7 +153,7 @@ file.onreadystatechange = function () {
 
         var rank = document.createTextNode(i + 1)
         var link = document.createElement('a')
-        var title = document.createTextNode(mydata.polls[i].title)
+        var title = document.createTextNode(mydata.polls[i].url)
         link.appendChild(title)
         link.href = mydata.polls[i].url
         var date = document.createTextNode(mydata.polls[i].date)
