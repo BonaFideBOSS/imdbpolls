8ec3eca64aae9fb507bd10390f713b21ef1cc540
BONAFIDE BOSS <amaanalmir@gmail.com>
2022-01-05 07:06:37 -0500

    update

--- pollData/Dan's Poll Data/dantomy.py
+++ pollData/Dan's Poll Data/dantomy.py
@@ -5,12 +5,12 @@ from bs4 import BeautifulSoup
 
 # ===== Read =====
 print("-----> Reading dan's data data.")
-file = open("danspolldata.json", "r")
+file = open("needrepair.json", "r")
 dandata = json.load(file)
 file.close()
 
 print("-----> Reading my poll data.")
-file = open("allimdbpolls.json", "r")
+file = open("../allimdbpolls.json", "r")
 mydata = json.load(file)
 file.close()
 
@@ -19,13 +19,6 @@ totalmypolls = len(mydata["polls"])
 print("-----> Total Polls on Dan's data: " + str(totaldanpolls))
 print("-----> Total Polls on my data: " + str(totalmypolls))
 
-count = 0
-for i in mydata["polls"]:
-    if "2020" in i["date"]:
-        if i["url"] not in dandata:
-            count = count + 1
-            print(i["url"])
-print(count)
 
 # print("-----> Saving")
 # file = open("allimdbpolls.json", "w")
--- pollData/imdbpollreport.py
+++ pollData/imdbpollreport.py
@@ -41,7 +41,7 @@ errorpolls = []
 for i in data["polls"]:
     pollLink = i["url"]
     if i["status"] == "Live":
-        if "ur75779804" in i["authorid"]:
+        if i["votes"] == "":
             try:
                 resultURL = pollLink
                 mobileURL = pollLink.replace("www", "m")
