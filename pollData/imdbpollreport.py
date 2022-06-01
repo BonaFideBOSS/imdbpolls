ebaff863245d94e2651489e29ba02c4129d2579f
BONAFIDE BOSS <amaanalmir@gmail.com>
2022-06-01 14:59:48 -0400

    Update imdbpollreport.py

--- pollData/imdbpollreport.py
+++ pollData/imdbpollreport.py
@@ -41,7 +41,7 @@ errorpolls = []
 for i in data["polls"]:
     pollLink = i["url"]
     if i["status"] == "Live":
-        if i["votes"] == "" or i["url"] in errorpolls:
+        if i["votes"] == "" or i["url"] in errorpolls or i["status"] == "Live":
             try:
                 resultURL = pollLink
                 mobileURL = pollLink.replace("www", "m")
